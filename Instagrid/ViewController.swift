6fa2ffb536d44d3e5b570d210cea6502dc2be727
Gilles Sagot <gilles.sagot@outlook.com>
2021-02-09 16:54:27 +0100

    UI : add a 'Swipe' label and an Arrow icon

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -9,29 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    @IBOutlet var arrow: UIButton!
     @IBOutlet weak var composition: CompoView!
     @IBOutlet weak var selectedImage: UIImageView!
     @IBOutlet weak var layoutButtonLeft: UIButton!
     @IBOutlet weak var layoutButtonMiddle: UIButton!
     @IBOutlet weak var layoutButtonRight: UIButton!
     @IBOutlet weak var selectedLayoutConstraintX: NSLayoutConstraint!
-
     @IBOutlet weak var selectedLayoutConstraintY: NSLayoutConstraint!
+    
+    let imageLeft = UIImage(named: "Arrow Left")
+    let imageUp = UIImage(named: "Arrow Up")
+    
+    var landscapeMode:Bool?
+    
+    var authorizedGesture:Bool?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
+        
+        
         buildInsideViewController()
-        selectedImage.frame = layoutButtonLeft.frame
-        selectedLayoutConstraintX.constant = 30
-        
+
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragImageLayout(_:)))
-        
         composition.addGestureRecognizer(panGestureRecognizer)
         
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if layoutButtonMiddle.center.x > composition.center.x {
+            arrow.setBackgroundImage(imageLeft, for: .normal)
+            landscapeMode = true
+        }
+        else{
+            arrow.setBackgroundImage(imageUp, for: .normal)
+            landscapeMode = false
+        }
+    }
+
+    
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if UIDevice.current.orientation.isLandscape {
+            arrow.setBackgroundImage(imageLeft, for: .normal)
+            landscapeMode = true
+        } else {
+            arrow.setBackgroundImage(imageUp, for: .normal)
+            landscapeMode = false
+        }
     }
     
     func buildInsideViewController() {
@@ -66,6 +96,7 @@ class ViewController: UIViewController {
     }
     
     @objc func dragImageLayout(_ sender: UIPanGestureRecognizer) {
+        /*
         switch sender.state {
         case .began, .changed:
             moveCompositionViewWith(gesture: sender)
@@ -74,19 +105,32 @@ class ViewController: UIViewController {
         default:
             break
         }
+        */
+        if sender.state == .began || sender.state == .changed {
+            moveCompositionViewWith(gesture: sender)
+        }
+        else if (sender.state == .ended || sender.state == .cancelled) && authorizedGesture==true{
+            shareCompositionView()
+        }
         
     }
     
     private func moveCompositionViewWith(gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: composition)
-
         // switch UIDevice.current.orientation
 
-        if layoutButtonMiddle.center.x > composition.center.x {
+        if landscapeMode == true && translation.x < -0 {
             composition.transform = CGAffineTransform(translationX: translation.x, y: 0)
+            authorizedGesture = true
         }
-        else {
+        else if landscapeMode == false && translation.y < -0 {
             composition.transform = CGAffineTransform(translationX: 0, y: translation.y)
+            authorizedGesture = true
+        }
+        else
+        {
+            authorizedGesture = false
+            
         }
     
     }
