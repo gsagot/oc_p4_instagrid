2a181eb9e7ec8fa8dec97d55dea6eb753961a465
Gilles Sagot <gilles.sagot@outlook.com>
2021-02-09 16:58:05 +0100

    Grid translation : Only for up and left

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+    
         
         buildInsideViewController()
 
